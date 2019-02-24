9fa9780f387d54f437c43efaf4cc8c4b3321353c
Cailyn Johnson <cailynjohnson@dhcp-10-105-208-36.wireless.northwestern.private>
2019-02-24 11:38:01 -0600

    DFS

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -33,8 +33,56 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### student code goes here
-        pass
+
+        # create inner tuples for each peg
+        peg1Tuple = tuple()
+        peg2Tuple = tuple()
+        peg3Tuple = tuple()
+
+        # begin by checking the smallest disk
+        currDisk = 1
+
+        while (True):
+
+            diskFound = False
+
+            # check every fact in the knowledge base
+            for currFact in self.kb.facts:
+                
+                # if the fact indicates a disk is on a peg
+                if currFact.statement.predicate == "on":
+                    
+                    # get the disk and peg numbers
+                    disk = currFact.statement.terms[0].term.element
+                    peg = currFact.statement.terms[1].term.element
+                    diskInt = int(disk[-1])
+                    pegInt = int(peg[-1])
+
+                    # if the disk number is the disk we're checking
+                    if diskInt == currDisk:
+
+                        diskFound = True
+                        
+                        # add tuple of disk number to corresponding peg number tuple
+                        if pegInt == 1:
+                            peg1Tuple += tuple([diskInt])
+                        elif pegInt == 2:
+                            peg2Tuple += tuple([diskInt])
+                        elif pegInt == 3:
+                            peg3Tuple += tuple([diskInt])
+
+                        # check next largest disk
+                        currDisk += 1
+                        
+                        break
+
+            # if no other disks are found
+            if diskFound == False:
+                
+                # return a tuple of tuples
+                return (peg1Tuple, peg2Tuple, peg3Tuple)
+
+
 
     def makeMove(self, movable_statement):
         """
@@ -52,8 +100,55 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
-        pass
+
+        # CREATION AND ASSIGNMENT
+        # assigns variables to movable statement terms
+        terms = movable_statement.terms
+        movableDisk = str(terms[0])
+        initialPeg = str(terms[1])
+        targetPeg = str(terms[2])
+        currState = self.getGameState()
+
+        # creates empty lists for facts that must be retracted and asserted as a result of the move
+        retractFacts = []
+        assertFacts = []
+
+
+        # INITIAL PEG CHANGES
+        # retracts 'on' and 'top' initial peg facts
+        retractFacts.append(parse_input("fact: (on " + movableDisk + " " + initialPeg + ")"))
+        retractFacts.append(parse_input("fact: (top " + movableDisk + " " + initialPeg + ")"))
+
+        initialPegTuple = currState[int(initialPeg[-1]) - 1]
+        if len(initialPegTuple) > 1:
+            assertFacts.append(parse_input("fact: top disk" + str(initialPegTuple[1]) + " " + initialPeg + ")"))
+        else:
+            assertFacts.append(parse_input("fact: (empty " + initialPeg + ")"))
+        
+
+        # TARGET PEG CHANGES
+        # asserts 'on' and 'top' target peg facts 
+        assertFacts.append(parse_input("fact: (on " + movableDisk + " " + targetPeg + ")"))
+        assertFacts.append(parse_input("fact: (top " + movableDisk + " " + targetPeg + ")"))
+
+        targetPegTuple = currState[int(targetPeg[-1]) - 1] 
+        if len(targetPegTuple) == 0: 
+            retractFacts.append(parse_input("fact: (empty " + targetPeg + ")"))
+        else:
+            retractFacts.append(parse_input("fact: top disk" + str(targetPegTuple[0]) + " " + targetPeg + ")"))
+
+
+        # RETRACT AND ASSERT
+        # retracts each fact that must be retracted from the KB as a result of the move
+        for currFact in retractFacts:
+            self.kb.kb_retract(currFact)
+
+        # asserts each fact that must be asserted into the KB as a result of the move
+        for currFact in assertFacts:
+            self.kb.kb_assert(currFact)
+        
+
+
 
     def reverseMove(self, movable_statement):
         """
@@ -99,8 +194,70 @@ class Puzzle8Game(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### Student code goes here
-        pass
+
+        # create inner tuples for each row
+        row1Tuple = tuple()
+        row2Tuple = tuple()
+        row3Tuple = tuple()
+
+        # for rows 1, 2, and 3
+        for currRow in range(1,4): 
+
+            # for columns 1, 2, and 3
+            for currColumn in range(1,4):
+
+                tileFound = False
+
+                # check every fact in the knowledge base
+                for currFact in self.kb.facts:
+
+                    # if the fact indicates the position of a tile
+                    if currFact.statement.predicate == "posn":
+
+                        # get the tile, column, and row terms 
+                        tile = currFact.statement.terms[0].term.element
+                        column = currFact.statement.terms[1].term.element
+                        row = currFact.statement.terms[2].term.element
+
+                        # if the tile is not empty, get the tile, column, and row numbers
+                        if tile != "empty":
+                            tileInt = int(tile[-1])
+                            columnInt = int(column[-1])
+                            rowInt = int(row[-1])
+
+                        # else the tile must be empty, so assign its tile number to -1 and get the column and row numbers
+                        else:
+                            tileInt = -1
+                            columnInt = int(column[-1])
+                            rowInt = int(row[-1])
+
+                        # if the column number and row number we're checking is the column and row we're checking
+                        if columnInt == currColumn and rowInt == currRow:
+
+                            tileFound = True
+
+                            # add tuple of tile number to corresponding row number tuple
+                            if rowInt == 1:
+                                row1Tuple += tuple([tileInt])
+                            elif rowInt == 2:
+                                row2Tuple += tuple([tileInt])
+                            elif rowInt == 3:
+                                row3Tuple += tuple([tileInt])
+
+                            break
+
+                if tileFound == False:
+
+                    if currRow == 1:
+                        row1Tuple += tuple([-1])
+                    elif currRow == 2:
+                        row2Tuple += tuple([-1])
+                    elif currRow == 3:
+                        row3Tuple += tuple([-1])
+
+        # return a tuple of tuples 
+        return (row1Tuple, row2Tuple, row3Tuple)
+
 
     def makeMove(self, movable_statement):
         """
@@ -118,8 +275,41 @@ class Puzzle8Game(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
-        pass
+        # CREATION AND ASSIGNMENT
+        # assigns variables to movable statement terms
+        terms = movable_statement.terms
+        movableTile = terms[0].term.element
+        initialX = terms[1].term.element
+        initialY = terms[2].term.element
+        targetX = terms[3].term.element
+        targetY = terms[4].term.element
+
+        # creates empty lists for facts that must be retracted and asserted as a result of the move
+        retractFacts = []
+        assertFacts = []
+
+
+        # INITIAL PEG CHANGES
+        # retracts facts with old positions for moved tile and empty tile
+        assertFacts.append(parse_input("fact: (posn  empty " + initialX + " " + initialY + ")"))
+        retractFacts.append(parse_input("fact: (posn " + movableTile + " " + initialX + " " + initialY + ")"))
+
+
+        # TARGET PEG CHANGES
+        # asserts facts with new positions for moved tile and empty tile
+        retractFacts.append(parse_input("fact: (posn  empty " + targetX + " " + targetY + ")"))
+        assertFacts.append(parse_input("fact: (posn " + movableTile + " " + targetX + " " + targetY + ")"))
+
+
+        # RETRACTION AND ASSERTION
+        # retracts each fact that must be retracted from the KB as a result of the move
+        for currFact in retractFacts:
+            self.kb.kb_retract(currFact)
+
+        # asserts each fact that must be asserted into the KB as a result of the move
+        for currFact in assertFacts:
+            self.kb.kb_assert(currFact)
+
 
     def reverseMove(self, movable_statement):
         """
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -18,8 +18,45 @@ class SolverDFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
+
+        # if current state of the node is the desired solution state, return true (we don't have to search for a solution)
+        if self.currentState.state == self.victoryCondition:
+            return True
+
+        # otherwise go to the next unexplored state
+        nextMoves = self.gm.getMovables()
+
+        # assign variables to the current state and current depth of the node
+        currState = self.currentState 
+        currDepth = self.currentState.depth
+
+        # if the current state is not a leaf node and has unexplored states
+        if nextMoves != 0:
+            
+            # expand the node
+            for currMove in nextMoves:
+                self.gm.makeMove(currMove)
+                nextState = GameState(self.gm.getGameState(), currDepth + 1, currMove)
+                currState.children.append(nextState)
+                nextState.parent = currState
+                self.gm.reverseMove(currMove)
+
+            # for each child of the current node, move to child if it's not been visited and change current state to child
+            for currChild in currState.children:
+                if currChild not in self.visited:
+                    self.visited[currChild] = True
+                    self.gm.makeMove(currChild.requiredMovable)
+                    self.currentState = currChild
+
+                    break
+
+        # if the node doesn't have any more unexplored states, go back up the tree to continue searching 
+        else:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+
+        # if the desired solution state is never reached, return false
+        return False
+        
 
 
 class SolverBFS(UninformedSolver):
@@ -40,4 +77,5 @@ class SolverBFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         ### Student code goes here
+        
         return True
